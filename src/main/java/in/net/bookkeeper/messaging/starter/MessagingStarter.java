6cfb84ff131ca0ca9bd8d570a7978bc72c2c3d58
krganeshrajhan <krganeshrajhan@gmail.com>
2017-11-19 10:30:39 +0800

    Added Message starter

---
+++ src/main/java/in/net/bookkeeper/messaging/starter/MessagingStarter.java
@@ -0,0 +1,20 @@
+package in.net.bookkeeper.messaging.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by krganeshrajhan on 18/11/17.
+ */
+@SpringBootApplication
+public class MessagingStarter {
+
+    /*@RequestMapping("/")
+    public String home() {
+        return "Hello World!";
+    }*/
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(MessagingStarter.class, args);
+    }
+}
